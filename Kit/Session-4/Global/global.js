694336b0729fc02f6395d4ed44cbf700dbc1bf00
Ashwin Hegde <ashwin.hegde3@gmail.com>
2014-04-05 00:38:03 +0530

    Adds examples for Globals related utility functions;
    Signed-off-by: Ashwin Hegde <ashwin.hegde3@gmail.com>

---
+++ Kit/Session-4/Global/global.js
@@ -0,0 +1,12 @@
+/***
+ * The goal of this file is to know about the Global functions;
+ *
+ *
+ * How to run this example:
+ * 1. node global.js
+ * 2. See the message get displayed on prompt.
+ */
+
+console.log(__filename);
+
+console.log(__dirname);
