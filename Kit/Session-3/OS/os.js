0dca3baafd6bfde6ec9d8e8c35af1e340ae9a43c
Ashwin Hegde <ashwinh@cybage.com>
2015-03-30 12:17:03 +0530

    Updates os e.g.

--- Kit/Session-3/OS/os.js
+++ Kit/Session-3/OS/os.js
@@ -22,52 +22,47 @@ console.info("Hostname is: %s", os.hostname());
 /***
  * Returns the name of the operating system.
 */
-console.log("Type is %s", os.type());
+console.info("Type is %s", os.type());
 
 /***
  * Returns the platform of the operating system.
 */
-console.log("Platform is %s", os.platform());
-
-/***
- * Returns the operating system CPU architecture.
-*/
-console.log("CPU Architecture is %s", os.arch());
-
-/***
- * Returns the operating system release.
-*/
-console.log("Release is %s", os.release());
-
-/***
- * Returns the operating system uptime in seconds.
-*/
-console.log("Uptime is %d Seconds", os.uptime());
-
-/***
- * Returns the operating system total amount of system memory in bytes.
-*/
-console.log("Total amount of system memory is %d in bytes", os.totalmem());
-
-/***
- * Returns the operating system the amount of free system memory in bytes.
-*/
-console.log("Total amount of free system memory is %d in bytes", os.freemem());
+console.info("Platform is %s", os.platform());
 
 /***
  * Returns the operating system's default directory for temp files.
 */
-console.log("Temp directory is", os.tmpDir());
+console.info("Default OS directory for temp files: %s", os.tmpdir());
+
+/***
+ * Returns the operating system CPU architecture.
+*/
+console.info("CPU Architecture is %s", os.arch());
+
+/***
+ * Returns the operating system release.
+*/
+console.info("Release is %s", os.release());
+
+/***
+ * Returns the operating system total amount of system memory in bytes.
+*/
+console.info("Total amount of system memory is %d in bytes", os.totalmem());
+
+/***
+ * Returns the operating system the amount of free system memory in bytes.
+*/
+console.info("Total amount of free system memory is %d in bytes", os.freemem());
 
 /***
  * Returns an array of objects containing information about each CPU/core installed:
  * model, speed (in MHz), and times
 */
-console.log("Information about CPU/Core Installed:\n");
-console.log(os.cpus());
+console.info("Information about CPU/Core Installed:\n");
+console.info(os.cpus());
 
 /***
  * Returns get a list of network interfaces:
 */
-console.log("List of network interfaces:\n");
-console.log(os.networkInterfaces());
+console.info("List of network interfaces:\n");
+console.info(os.networkInterfaces());
