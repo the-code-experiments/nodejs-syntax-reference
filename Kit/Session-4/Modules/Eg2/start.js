269fc7960e30840218544b135d8dbabdf685f2d6
Ashwin Hegde <ashwinh@cybage.com>
2015-03-31 14:16:09 +0530

    Removes Eg1 from modules

--- Kit/Session-4/Modules/Eg2/start.js
+++ Kit/Session-4/Modules/Eg2/start.js
@@ -1,19 +1,12 @@
 /***
  * The goal of this file is to know about the Mobules functions;
- *
+ * Here, we are trying to create a small NPM example.
  *
  * How to run this example:
  * 1. node start.js
  * 2. See the message get displayed on prompt.
  */
 
-var circle = require('./circle.js');
-console.log("The area of a circle of radius 4 is: " + circle.area(4));
-
-
-var square = require('./square.js');
+var square = require('./npm');
 var mySquare1 = square(2);
-var mySquare2 = square(4);
 console.log('The area of my square 1 is: ' + mySquare1.area());
-console.log('The area of my square 2 is: ' + mySquare2.area());
-
