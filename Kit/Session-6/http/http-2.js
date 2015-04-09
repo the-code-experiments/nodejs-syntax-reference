1cacadbf4dc1c215115a555329df97f423227e77
hegdeashwin <ashwin.hegde3@gmail.com>
2015-04-09 21:57:45 +0530

    includes http examples

---
+++ Kit/Session-6/http/http-2.js
@@ -0,0 +1,51 @@
+/***
+ * The goal of this file is to know How to http function and events;
+ *
+ *
+ * How to run this example:
+ * 1. node http-2.js
+ * 2. See the message get displayed on prompt.
+ */
+
+// Your gateway page. It's a file that Node will serve if a directory is requested.
+var gatewayPage = "src/index.html";
+var listenPort = 5005;
+
+var http = require('http'),
+	fs = require('fs'),
+	path = require('path');
+
+var requestConnector = function(req, res) {
+
+	if (req.url === '/') {
+		req.url = gatewayPage;
+	}
+	var file = path.join(__dirname, req.url);
+
+	fs.exists(file, function(exists) {
+		if (exists) {
+			fs.stat(file, function(error, stats) {
+				if (error) {
+					throw error;
+				}
+				if (stats.isFile()) {
+					res.writeHead(200, {
+						'Content-Type': 'text/html'
+					});
+					fs.createReadStream(file).pipe(res);
+				} else {
+					res.writeHead(404);
+					res.end('Error 404 Not Found\nThe requested resource could not be found but may be available again in the future.');
+
+				}
+			});
+		} else {
+			res.writeHead(404);
+			res.end('Error 404 Not Found\nThe requested resource could not be found but may be available again in the future.');
+		}
+	});
+}
+
+http.createServer(requestConnector).listen(listenPort, function() {
+	console.log("\n\n\tServer is connected on localhost:" + listenPort);
+});
