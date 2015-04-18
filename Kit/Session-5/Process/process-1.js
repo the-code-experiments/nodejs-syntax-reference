aa7e5d9b9c9c0f79e73a1ec3bcb1349a0dce90e2
hegdeashwin <ashwin.hegde3@gmail.com>
2015-04-18 20:45:54 +0530

    Adds process-argv eg.

---
+++ Kit/Session-5/Process/process-1.js
@@ -0,0 +1,13 @@
+/***
+ * The goal of this file is to know about the process functions;
+ *
+ *
+ * How to run this example:
+ * 1. node process-1.js OR
+ * 1. node process-1.js one two three
+ * 2. See the message get displayed on prompt.
+ */
+
+process.argv.forEach(function(val, index, array) {
+	console.log(index + ": " + val);
+});
