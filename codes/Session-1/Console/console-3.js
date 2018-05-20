82d37aaeb59b7d54708b7c7d57451242e4769238
ashegde <ashegde@paypal.com>
2018-05-20 11:17:31 +0530

    update(example): console

--- codes/Session-1/Console/console-3.js
+++ codes/Session-1/Console/console-3.js
@@ -23,7 +23,6 @@ console.table([
   }
 ])
 
-/** Below are all inspector method only */
 /**
  * This method does not display anything unless used in the inspector.
  * Start the current JS CPU profiling with the optional label 
@@ -32,3 +31,14 @@ console.table([
 console.profile("Show_Performance");
 console.log('Hello World!');
 console.profileEnd();
+
+/**
+ * This method helps to create nesting block
+ */
+console.log("This is the level 1 message");
+console.group();
+  console.log("This is the level 2 message");
+  console.group();
+    console.log("This is the level 3 message");
+  console.groupEnd();
+console.groupEnd();
